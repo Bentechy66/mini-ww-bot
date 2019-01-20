2dc96405eedf4fbbd3202bc9dcfb061615180829
Calvin Edwards <calvin.588.edwards@gmail.com>
2019-01-20 18:56:20 +0000

    add command stubs

--- ccbot/usercmds.py
+++ ccbot/usercmds.py
@@ -22,6 +22,38 @@ class UserCommands:
         Usage: `]create channel-name @person1 @person2 @person3...`
         """
         await ctx.send("(TODO) creating cc {} with people {}".format(name, ", ".join(people)))
+    
+    @commands.command()
+    async def add(self, ctx, *people):
+        """Adds people to a conspiracy channel
+
+        Adds any number of people to the conspiracy channel it was sent in.
+        This is only available to the owner of that conspiracy channel.
+        Usage: `]add @person1 @person2 @person3...`
+        """
+        await ctx.send("(TODO) adding people {}".format(name, ", ".join(people)))
+    
+    @commands.command()
+    async def remove(self, ctx, *people):
+        """Removes people from a conspiracy channel
+
+        Removes any number of people from the conspiracy channel it was sent in.
+        This is only available to the owner of that conspiracy channel.
+        The owner of a conspiracy channel cannot be removed from that channel.
+        Usage: `]remove @person1 @person2 @person3...`
+        """
+        await ctx.send("(TODO) removing people {}".format(name, ", ".join(people)))
+    
+    @commands.command(name="list")
+    async def _list(self, ctx, *people):
+        """Lists people in a conspiracy channel
+
+        Lists all the people currently in the conspiracy channel it was sent in.
+        If the conspiracy channel is not a hidden channel then the owner will
+        be listed as well.
+        Usage: `]list`
+        """
+        await ctx.send("(TODO) listing".format(name, ", ".join(people)))
 
 
 def setup(bot):
