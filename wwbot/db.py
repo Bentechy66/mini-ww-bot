424ad990c69e73d28b4de690c935dee4300ad685
Calvin Edwards <calvin.588.edwards@gmail.com>
2019-02-01 17:13:06 +0000

    add polls to database

--- wwbot/db.py
+++ wwbot/db.py
@@ -11,10 +11,18 @@ class BaseModel(Model):
 
 class Player(BaseModel):
     # needs to store name and emoji
-    discord_id      = CharField(unique=True) # don't worry about length, sqlite doesn't care anyway
-    emoji           = CharField(unique=True)
+    discord_id      = IntegerField(unique=True) 
+    emoji           = CharField(unique=True)   # don't worry about length, sqlite doesn't care anyway
+
+# do it as 2 tables in case we need poll-wide info in future
+class Poll(BaseModel):
+    pass # no data here needed yet, other than default id
+
+class PollMessage(BaseModel):
+    discord_id      = IntegerField(unique=True) # discord id of the message in question
+    poll            = ForeignKeyField(Poll, backref="messages")
 
 def create_tables():
     with db:
         print("Creating database tables in {}...".format(conf['database']['filename']))
-        db.create_tables([Player])
+        db.create_tables([Player, Poll, PollMessage])
