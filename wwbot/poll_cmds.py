f92c120f5f67ec4b65933a1922d5fc289aec7c26
Calvin Edwards <calvin.588.edwards@gmail.com>
2019-02-02 21:33:07 +0000

    add stub commands

--- wwbot/poll_cmds.py
+++ wwbot/poll_cmds.py
@@ -1,9 +1,28 @@
+import discord
+from discord.ext import commands
 
+from wwbot.permissions import chk_gamemaster
+from wwbot.game_phase import needs_game_phase, GamePhases
 
 class PollCmds():
-    def __global_check():
-        # these are all admin cmds
-        pass # todo
+    def __init__(self, bot):
+        self.bot = bot
+
+    @needs_game_phase(GamePhases.GAME)
+    @chk_gamemaster
+    @commands.group()
+    async def poll(self, ctx):
+        pass
+    
+    @poll.command()
+    async def new(self, ctx):
+        # creates a poll with options of all alive players for voting for everyone in this channel.
+        pass
+    
+    @poll.command()
+    async def close(self, ctx, pollid : int):
+        # closes that poll, puts results somewhere
+        pass
 
 def setup(bot):
     bot.add_cog(PollCmds(bot))
