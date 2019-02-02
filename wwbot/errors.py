640bee227326cbb38bdb2860cae25fb32bc654ac
Calvin Edwards <calvin.588.edwards@gmail.com>
2019-02-02 16:57:30 +0000

    add more error types

--- wwbot/errors.py
+++ wwbot/errors.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
 
+from wwbot.game_phase import GamePhases
+
 class NoSuchChannel(Exception):
     # channel or category does not exist
     pass
@@ -15,4 +17,23 @@ class NotOwner(commands.CommandError):
     pass
 class EmojiInUse(Exception):
     # that emoji is already being used
+    pass
+
+class WrongGamePhase(commands.CheckFailure):
+    # wrong game phase.
+    def __init__(self, needs, current):
+        self.needs = GamePhases(needs)
+        self.current = GamePhases(current)
+    def __str__(self):
+        fstr = "Wrong game phase! We needed `{0.name}` ({0.value}) but instead we are on `{1.name}` ({1.value})."
+        return fstr.format(self.needs, self.current)
+
+class PermissionsError(Exception):
+    # base exeption for permission-related stuff
+    pass
+class NeedsGM(PermissionError):
+    # you need to be a gamemaster to do that
+    pass
+class NeedsParticipant(PermissionError):
+    # you need to be a participant
     pass
