9bd1f5fe52c705f2f75bd4d25fce2225aa41f1e4
Calvin Edwards <calvin.588.edwards@gmail.com>
2019-02-03 13:52:48 +0000

    add documentation for the poll commands

--- wwbot/poll_cmds.py
+++ wwbot/poll_cmds.py
@@ -13,15 +13,28 @@ class PollCmds():
     @chk_gamemaster
     @commands.group()
     async def poll(self, ctx):
+        """All commands to do with polls.
+
+        These are all gamemaster-only
+        """
         pass
     
     @poll.command()
     async def new(self, ctx):
+        """Create a new poll in the current channel.
+
+        All of the currently alive players will be options for voting.
+        """
         # creates a poll with options of all alive players for voting for everyone in this channel.
         await polls.create_poll(ctx.channel, polls.get_all_alive(self.bot))
     
     @poll.command()
     async def close(self, ctx, pollid : int):
+        """Close any open poll and print the raw results in the current channel.
+
+        pollid should be the id of the poll that was given to you by the ]poll new command.
+        This doesn't have to be run in the same channel that the poll was created in.
+        """
         # closes that poll, puts results somewhere
         reactions = await polls.close_poll(self.bot, pollid)
         await ctx.send("Results for poll #`{}`:".format(pollid))
