95406b1a121e965aa103efa781300882fd0dd0f5
Calvin Edwards <calvin.588.edwards@gmail.com>
2019-02-03 12:54:32 +0000

    implement closing of polls

--- wwbot/polls.py
+++ wwbot/polls.py
@@ -18,10 +18,28 @@ def get_all_alive(bot):
 async def create_poll(ch, options):
     # options should have properties discord_id and emoji
     poll = Poll.create(channel=ch.id)
-    for chunk in chunks(options, 20):
+    await ch.send("Poll #`{}`".format(poll.id))
+    for chunk in chunks(options, 1):
         # chunk is a list of 20 Players from db
         pollmsg = await ch.send("\n".join("{0.emoji} - <@{0.discord_id}>".format(opt) for opt in chunk))
         PollMessage.create(poll=poll, discord_id=pollmsg.id)
         for p in chunk:
             await pollmsg.add_reaction(p.emoji)
 
+async def close_poll(bot, pollid):
+    guild = fetch_guild(bot)
+    me = guild.me
+    poll = Poll.get_or_none(id=pollid)
+    if poll is None:
+        raise errors.NoSuchPoll()
+    reactions = {}
+    channel = bot.get_channel(poll.channel)
+    for dbmsg in poll.messages:
+        msg = await channel.get_message(dbmsg.discord_id)
+        for reaction in msg.reactions:
+            users = await reaction.users().flatten()
+            users.remove(me)
+            reactions[reaction.emoji] = users
+        await msg.clear_reactions()
+    poll.delete_instance(recursive=True)
+    return reactions
