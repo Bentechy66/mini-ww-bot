a933aff0b891436b85cfa998651ff348525a1114
Calvin Edwards <calvin.588.edwards@gmail.com>
2019-02-03 11:55:28 +0000

    create poll command done

--- wwbot/poll_cmds.py
+++ wwbot/poll_cmds.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 
 from wwbot.permissions import chk_gamemaster
 from wwbot.game_phase import needs_game_phase, GamePhases
+from wwbot.polls import create_poll, get_all_alive
 
 class PollCmds():
     def __init__(self, bot):
@@ -17,7 +18,7 @@ class PollCmds():
     @poll.command()
     async def new(self, ctx):
         # creates a poll with options of all alive players for voting for everyone in this channel.
-        pass
+        await create_poll(ctx.channel, get_all_alive(self.bot))
     
     @poll.command()
     async def close(self, ctx, pollid : int):
