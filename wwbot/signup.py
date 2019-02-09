e9fa9863425a8f5cdd3b5af9f861d6d42c21f006
Calvin Edwards <calvin.588.edwards@gmail.com>
2019-02-09 16:12:02 +0000

    signup things: better list_signedup, unsignup, role giving-out

--- wwbot/signup.py
+++ wwbot/signup.py
@@ -5,16 +5,19 @@ from wwbot import errors
 from wwbot.util import is_emoji_str, fetch_guild
 from wwbot.game_phase import needs_game_phase, GamePhases
 from wwbot.db import db, Player
+from wwbot.config import conf
+from wwbot.permissions import chk_gamemaster
 
 def signup_or_change(member, emoji):
-    # returns bool (worked or not) and str (message to print)
+    # returns bool (new or not) and str (message to print)
     # first check if someone is already using that emoji
     p = Player.get_or_none(Player.emoji == emoji)
     if p is not None:
+        msg = ":warning: "
         if p.discord_id == member.id:
-            msg = "You are"
+            msg += "You are"
         else:
-            msg = "Someone else is"
+            msg += "Someone else is"
         return False, msg+" already using the emoji {}!".format(emoji)
 
     p = None
@@ -22,14 +25,23 @@ def signup_or_change(member, emoji):
     if p is None:
         # create the player and add it
         Player.create(discord_id=member.id, emoji=emoji)
-        return True, "{} signed up with emoji {}".format(member.mention, emoji)
+        return True, ":white_check_mark: {} signed up with emoji {}".format(member.mention, emoji)
     else:
         # update the player
         p.emoji = emoji
         p.save()
         # well that was easy
-        return True, "{} changed their emoji to {}.".format(member.mention, emoji)
+        return False, ":white_check_mark: {} changed their emoji to {}.".format(member.mention, emoji)
 
+async def remove_player(member, bot):
+    p = Player.get_or_none(Player.discord_id == member.id)
+    if p == None:
+        return False
+    p.delete_instance()
+    guild = fetch_guild(bot)
+    participant = guild.get_role(conf['ids'].getint("participant"))
+    await member.remove_roles(participant)
+    return True
 
 class SignupCmds:
     def __init__(self, bot):
@@ -41,18 +53,37 @@ class SignupCmds:
         if not is_emoji_str(emoji):
             await ctx.send("{} is not an emoji!".format(emoji))
             return
-        worked, msg = signup_or_change(ctx.author, emoji)
-        if worked:
-            status = ":white_check_mark: "
-        else:
-            status = ":warning: "
-            
-        await ctx.send(status + msg)
+        needs_role, msg = signup_or_change(ctx.author, emoji)
+        if needs_role:
+            guild = fetch_guild(self.bot)
+            role = guild.get_role(conf['ids'].getint("participant"))
+            await ctx.author.add_roles(role)
+        await ctx.send(msg)
     
+    @needs_game_phase(GamePhases.SIGNUP)
+    @chk_gamemaster()
+    @commands.command()
+    async def unsignup(self, ctx, who: discord.Member):
+        worked = await remove_player(who, self.bot)
+        if worked:
+            await ctx.send(":white_check_mark: Player {} is no longer signed up".format(who))
+        else:
+            await ctx.send("That person wasn't signed up anyway so I didn't do anything.")
+
+
     @commands.command()
     async def list_signedup(self, ctx):
         guild = fetch_guild(self.bot)
-        await ctx.send("\n".join("{} - {}".format(p.emoji, guild.get_member(p.discord_id)) for p in Player.select()))
+        players = list(Player.select())
+        count = len(players)
+        if count == 0:
+            await ctx.send("No one has signed up yet!")
+        else:
+            await ctx.send(
+                "*Total: {} players*\n".format(count) + 
+                "\n".join(
+                    "{} - {}".format(p.emoji, guild.get_member(p.discord_id).display_name) for p in players
+                ))
 
 def setup(bot):
     bot.add_cog(SignupCmds(bot))
