6b3daec7122a8594df2db8f9a3ca531b0ab4eee1
Calvin Edwards <calvin.588.edwards@gmail.com>
2019-02-09 16:48:27 +0000

    start game command to add role to all players

--- wwbot/game_phase_cmds.py
+++ wwbot/game_phase_cmds.py
@@ -3,6 +3,9 @@ from discord.ext import commands
 
 from wwbot.game_phase import game_phase, set_game_phase, GamePhases
 from wwbot.permissions import chk_gamemaster
+from wwbot.db import Player
+from wwbot.util import fetch_guild
+from wwbot.config import conf
 
 class GamePhaseCmds:
     def __init__(self, bot):
@@ -27,6 +30,24 @@ class GamePhaseCmds:
                 return
         set_game_phase(int_to)
         await ctx.send(":white_check_mark: Game Phase is now `{0.name}` ({0.value})".format(GamePhases(game_phase())))
+
+    @chk_gamemaster()
+    @commands.check(lambda _: game_phase() != GamePhases.GAME)
+    @commands.command()
+    async def start_game(self, ctx):
+        """Assigns the participant role to all players and changes game phase.
+
+        This is equivalent to manually assigning the Participant role to all
+        signed up players, then doing `{PREFIX}gamephase set GAME`.
+        """
+        guild = fetch_guild(self.bot)
+        participant = guild.get_role(conf['ids'].getint("participant"))
+        for p in Player.select():
+            m = guild.get_member(p.discord_id)
+            await m.add_roles(participant)
+            await ctx.send("{} is now a participant...".format(m.mention))
+        set_game_phase(GamePhases.GAME)
+        await ctx.send("Set game phase to `GAME` ({})".format(GamePhases.GAME.value))
 
 def setup(bot):
     bot.add_cog(GamePhaseCmds(bot))
