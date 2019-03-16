1f1d789b00d54cfc3f7edd4095e260ec79a78935
Calvin Edwards <calvin.588.edwards@gmail.com>
2019-03-16 14:15:08 +0000

    migration script because of circumstances

--- housekeeping.py
+++ housekeeping.py
@@ -3,6 +3,18 @@ import sys
 from wwbot.db import create_tables
 from wwbot.config import conf
 
+def m_role_column():
+    "add the role column to player table"
+    from playhouse.migrate import migrate, SqliteMigrator
+    from peewee import CharField
+    from wwbot.db import db
+    db.connect()
+    field = CharField(default="")
+    migrator = SqliteMigrator(db)
+    migrate(
+        migrator.add_column('player', 'role', field)
+    )
+
 def main():
     if len(sys.argv) != 2:
         sys.exit("Please specify the command you want me to run.")
@@ -11,6 +23,11 @@ def main():
         print("Creating db tables")
         create_tables()
         print("All done!")
+
+    elif sys.argv[1] == "m_role_column":
+        print("migration - adding role column to Player table")
+        m_role_column()
+        print("All Done!")
     
     else:
         sys.exit("I don't know what you want me to do!")
