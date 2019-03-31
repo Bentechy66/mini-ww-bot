179f1f2034c38d7304d7e19373ed53d4fb90b6ff
Calvin Edwards <calvin.588.edwards@gmail.com>
2019-03-31 18:53:02 +0100

    quick fix for when there aren't any alive participants

--- wwbot/mentioning.py
+++ wwbot/mentioning.py
@@ -7,12 +7,18 @@ from wwbot.db import Player
 from wwbot.permissions import is_participant
 
 class PlayerConverter(commands.MemberConverter):
+    def __init__(self, needs_alive=True):
+        super().__init__()
+        self.needs_alive = needs_alive
+    
     async def convert(self, ctx, arg):
         player = Player.get_or_none(Player.emoji == arg)
         if player is None:
             member =  await super().convert(ctx, arg)
         else:
             member = ctx.guild.get_member(player.discord_id)
+        if not self.needs_alive:
+            return member
         if is_participant(member):
             return member
         else:
