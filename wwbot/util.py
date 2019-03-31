664b5eed3717a2ed6fc8eaedb7ee1cb7f210934f
Calvin Edwards <calvin.588.edwards@gmail.com>
2019-03-31 18:32:27 +0100

    add confirm fn

--- wwbot/util.py
+++ wwbot/util.py
@@ -5,6 +5,8 @@ from discord.ext import commands
 import emoji
 from wwbot.config import conf
 
+import asyncio
+
 def is_emoji_str(s):
     # returns whether s is a string containing nothing but a single emoji
     er = emoji.get_emoji_regexp()
@@ -17,3 +19,19 @@ def chunks(source, length):
     # yields successive n-sized chunks from l
     for i in range(0, len(source), length):
         yield source[i : i+length]
+
+async def confirm(ctx, message="This is a dangerous operation!", timeout=10.0):
+    # makes sure a human types "ok" in a channel
+    # returns true if successful
+    def check(m):
+        return m.author.id == ctx.author.id and m.content.lower() == "ok" and m.channel == ctx.channel
+    
+    await ctx.send("{}\n*Please type 'ok' here to confirm. This will expire in {} seconds.*".format(message, timeout))
+    try:
+        msg = await ctx.bot.wait_for("message", check=check, timeout=timeout)
+    except asyncio.TimeoutError:
+        await ctx.send("Timed out.")
+        return False
+    else:
+        return True
+
